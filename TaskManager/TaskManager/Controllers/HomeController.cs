907c9983805fb206973fa29dce2483f4d4fae913
Moises <moises007@gmail.com>
2019-04-01 17:26:01 -0500

    ActionResult

--- TaskManager/TaskManager/Controllers/HomeController.cs
+++ TaskManager/TaskManager/Controllers/HomeController.cs
@@ -9,9 +9,9 @@ namespace TaskManager.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello TaskManager";
+            return View();
         }
     }
 }
