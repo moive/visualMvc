f3857263d9023f46c1c17348b8f1f4218c52dfcc
Moises <moises007@gmail.com>
2019-04-10 18:12:25 -0500

    add Index Action and cleaning

--- TaskManager/TaskManager/Controllers/HomeController.cs
+++ TaskManager/TaskManager/Controllers/HomeController.cs
@@ -27,56 +27,9 @@ namespace TaskManager.Controllers
         /// <summary>
         /// Get list of tasks
         /// </summary>
-        
-        public ActionResult Index(int? start, int? length, int? id)
+        public ActionResult Index()
         {
-            return RedirectToAction("GetTaskById", new { id = id });
-
-            List<TaskDTO> tasks = _Tasks;
-
-            if (start.HasValue) {
-                tasks = tasks.Skip(start.Value).ToList();
-            }
-
-            if (length.HasValue) {
-                tasks = tasks.Take(length.Value).ToList();
-            }
-
-
-            StringBuilder content = new StringBuilder();
-
-            foreach (TaskDTO task in tasks) {
-                content.Append(string.Format("<div>Id: {0}, Name: {1}</div>", task.ID, task.Name));
-            }
-
-            return new ContentResult()
-            {
-                Content = content.ToString()
-            };
-        }
-
-        /// <summary>
-        /// Print a task by task ID
-        /// </summary>
-        
-        public ActionResult GetTaskById(int? id) {
-
-            if (!id.HasValue)
-            {
-                return new ContentResult()
-                {
-                    Content = "<div>ID parameter is required!</div>"
-                };
-            }
-
-            TaskDTO task = _Tasks.Where(t => t.ID == id).FirstOrDefault();
-
-            return new ContentResult()
-            {
-                Content = task == null ?
-                string.Format("<div>Task {0} not found</div>", id) :
-                string.Format("<div>Id: {0}, Name: {1}</div>", task.ID, task.Name)
-            };
+            return View();
         }
     }
 }
