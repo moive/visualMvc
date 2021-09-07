27088c5574a3e8e9fa1de52e8ff008cccd968cec
Moises Velasquez <mvelasquez@pictoric.com>
2021-09-07 00:11:04 -0500

    ording and taskQueryServices

---
+++ TaskManager/TaskManager/Tasks/GlobalDbContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TaskManager.Tasks
+{
+    internal class GlobalDbContext
+    {
+        public List<TaskDTO> Tasks { get; internal set; }
+    }
+}
---
+++ TaskManager/TaskManager/Tasks/TaskQueryServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TaskManager.Services;
+
+namespace TaskManager.Tasks
+{
+    public class TaskQueryServices
+    {
+        private GlobalDbContext _context;
+
+        public TaskQueryServices()
+        {
+            _context = new GlobalDbContext();
+        }
+        public List<TaskDTO> GetTasks()
+        {
+            return _context.Tasks.ToList();
+        }
+    }
+}
