fa05cd8152fb9d301f669e722626f4a2a2c57f36
Moises Velasquez <mvelasquez@pictoric.com>
2021-09-06 23:01:59 -0500

    working with models

--- TaskManager/TaskManager/Controllers/HomeController.cs
+++ TaskManager/TaskManager/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using TaskManager.Models;
 
 namespace TaskManager.Controllers
 {
@@ -23,14 +24,19 @@ namespace TaskManager.Controllers
                 new TaskDTO(){ID=3, Name="Llamar a mami", DueDate = DateTime.Now.AddDays(-1)}
             };
         }
-        
+
         /// <summary>
         /// Get list of tasks
         /// </summary>
         public ActionResult Index()
         {
-            ViewBag.ListOfTasks = _Tasks;
-            return View();
+            TaskList model = new TaskList()
+            {
+                ListOfTasks = _Tasks
+
+            };
+
+            return View(model);
         }
 
         /// <summary>
---
+++ TaskManager/TaskManager/Models/TaskList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Models
+{
+    public class TaskList
+    {
+        public List<TaskDTO> ListOfTasks { get; set; }
+    }
+}
