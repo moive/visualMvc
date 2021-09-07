fae6afc3ec0f53456a18d95d076a825fbe379c15
Moises Velasquez <mvelasquez@pictoric.com>
2021-09-06 23:30:51 -0500

    configuration entity framework

---
+++ TaskManager/TaskManager/Services/GlobalDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Services
+{
+    public class GlobalDBContext: DbContext
+    {
+        public GlobalDBContext() : base("mgcDb")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new TaskModel());
+        }
+    }
+}
